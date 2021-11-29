1ebc9c69fdc7923a937499ec6d769593b6c5a4d5
João Augusto <johhdq@hotmail.com>
2021-11-29 16:39:51 -0300

    teste

--- source/repos/WorkDay/Criado/Entities/User.cs
+++ source/repos/WorkDay/Criado/Entities/User.cs
@@ -4,15 +4,25 @@ using System.Text;
 
 namespace Criado.Entities
 {
-    class User
+    public class User
     {
         public string Name { get; set; }
+        public string NickName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public List<WorkItem> WorkItems { get; set; }
 
-        
+        public User(string name, string nickName, string email, string password)
+        {
+            Name = name;
+            NickName = nickName;
+            Email = email;
+            Password = password;
+        }
 
-
-
+        public override string ToString()
+        {
+            return $"Name >> {Name}; NickName >> {NickName}; Email >> {Email}";
+        }
     }
 }
